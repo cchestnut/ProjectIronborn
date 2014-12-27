9402ce5db95ef6e4a31b918850489baa2ce0333a
cchestnut <cchest01@villanova.edu>
2014-12-26 19:57:19 -0500

    initial commit

---
+++ Assets/Standard Assets/Scripts/General Scripts/InputMapping.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+	var inputKey = Input.inputString;
+	if (inputKey == 'x'){
+		animation.Play("punch_hi_left");
+	}
+}
+
+function OnCollisionEnter (){
+}
