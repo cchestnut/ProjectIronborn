b58b0f0f2552cd531f5a491fb105b40aa696cf33
cchestnut <christopher.chestnut2@gmail.com>
2014-12-27 13:02:52 -0500

    collision detection and enemy impact animation

---
+++ Assets/ImpactDetector.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnCollisionEnter (col : Collision)
+{
+	if(col.gameObject.name == "roBot"){
+    	this.animation.Play("xhit_body");
+    	Debug.Log("worked");
+    } else {
+    	Debug.Log(col.gameObject.name);
+    }
+}
+
+function OnTriggerStay (other: Collider){
+	var otherBot = other.gameObject;
+	if(otherBot.name == "roBot" && otherBot.animation.IsPlaying("punch_hi_left")){
+		this.animation.Play("xhit_body");
+	} 
+}
